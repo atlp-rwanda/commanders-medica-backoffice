b7979113c05268d8a017b60891790ace75c65ad9
justinmihigo <mihigojustin28@gmail.com>
2024-06-29 15:30:21 +0200

    ft(create admin dashboard) a add navbar top menu and the main section
    ft(add responsiveness and create component for the topbar)
    
    fix (typo and font size and weights)
    
    ft(rebase from main)
    
    ft(fetch doctors info from doctors schema)
    
    ft load the image profile from the docors table
    
    fix the background color conflict

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'vaxjykeihsernxqxeyiy.supabase.co',
+          port: '',
+        },
+      ],
+    },
+}
 export default nextConfig;
