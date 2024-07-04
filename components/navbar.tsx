ea2bf8cb89c2387ad36c50fe34f508e38fb410a9
justinmihigo <mihigojustin28@gmail.com>
2024-07-04 15:59:56 +0200

    fix (width issues)

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { combineChunks } from "@supabase/ssr";
 import Image from "next/image"
+import Link from "next/link";
 export default function Navbar() {
     const handleHide = () => {
         // const hamburger= document.querySelector('#menu')
@@ -18,24 +19,24 @@ export default function Navbar() {
                 <div className="mb-[50px] p-2" id='menu' onClick={handleHide}>
                     <Image src={require("../assets/icons/menu.svg")} alt="menu" width={30} height={30} />
                 </div>
-                <div className="flex gap-[30px]  items-center hover:bg-[#FFFFFF40] rounded-xl p-2">
+                <Link href={'/dashboard'} className="flex gap-[30px]  items-center hover:bg-[#FFFFFF40] rounded-xl p-2">
                     <div className="w-[30px]">
                         <span><Image src={require("../assets/icons/home.svg")} alt="home" /> </span>
                     </div>
                     <span className="desc">Dashboard</span>
-                </div>
+                </Link>
                 <div className="flex gap-[30px] items-center hover:bg-[#FFFFFF40] rounded-xl p-2">
                     <div className="w-[30px]">
                         <span><Image src={require("../assets/icons/calendar.svg")} alt="home" /> </span>
                     </div>
                     <span className="desc">Calendar</span>
                 </div>
-                <div className="flex gap-[30px] items-center hover:bg-[#FFFFFF40] rounded-xl p-2">
+                <Link href={'/messages'} className="flex gap-[30px] items-center hover:bg-[#FFFFFF40] rounded-xl p-2">
                     <div className="w-[30px]">
                         <span><Image src={require("../assets/icons/messages.svg")} alt="home" /> </span>
                     </div>
                     <span className="desc">Messages</span>
-                </div>
+                </Link>
                 <div className="flex gap-[30px] items-center hover:bg-[#FFFFFF40] rounded-xl p-2">
                     <div className="w-[30px]">
                         <span><Image src={require("../assets/icons/reports.svg")} alt="home" /> </span>
